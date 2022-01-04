a9b28ee89cd77a725fc1fa9c09a77939bff3ce05
AK Krajewska <akrajewska@gmail.com>
2022-01-04 01:06:30 +0000

    Change json response case depending on env style variable

--- myApp.js
+++ myApp.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var app = express();
+
+
 console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
   });
 app.get("/json", function(req, res) {
-    res.json({
-      "message": "Hello json"
-      }); 
+    var response = '"Hello json"';
+    if (process.env['MESSAGE_STYLE'] === "uppercase") {
+  res.json({
+    "message": response.toUpperCase()});
+} else {
+  res.json({
+    "message": response}); 
+}
 });
+
+
+
+
 
 
 
