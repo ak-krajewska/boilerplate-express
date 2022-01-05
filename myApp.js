27c8c00c5fb87ad078a3cf2e66001909c6fe5c4d
AK Krajewska <akrajewska@gmail.com>
2022-01-05 06:24:13 +0000

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -6,10 +6,25 @@ console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
 
 //Challenge: Implement a Root-Level Request Logger Middleware
+/*
 app.use("/", function (req, res, next) {
   console.log(req.method + " " + req.path + " - " + req.ip)
   next()
-})
+});
+*/
+
+//Challenge: Chain Middleware to Create a Time Server
+app.get('/now', function (req, res, next) {
+  //add new middleware property to request object
+  req.time = new Date().toString();
+  next();
+},
+  function (req, res) {
+  //access new property and output json
+  res.send({
+    time : req.time
+  });
+});
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
