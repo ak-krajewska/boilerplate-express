a53a9cd9d42d346c094f0ea411e6df4bb48352b6
AK Krajewska <akrajewska@gmail.com>
2022-01-05 05:27:41 +0000

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -4,6 +4,15 @@ var app = express();
 
 console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
+
+//Challenge: Implement a Root-Level Request Logger Middleware
+app.use("/", function (req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip
+    //method path - ip.
+    )
+  next()
+})
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
   });
@@ -15,6 +24,7 @@ app.get("/json", function(req, res) {
   res.json({
     "message": response});
  });
+
 
 
 
